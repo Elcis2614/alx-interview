843cebfea71a04e51c702201323aa8d2d023d758
elcis2614 <elisereflorbusole@gmail.com>
2023-09-28 04:52:26 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
-  from math import comb
+    from math import comb
 
-  triangle = []
-
-  for i in range(n):
-      row = []
-      for j in range(i + 1):
-          row.append(comb(i, j))
-      triangle.append(row)
-  return triangle
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            row.append(comb(i, j))
+            triangle.append(row)
+    return triangle
