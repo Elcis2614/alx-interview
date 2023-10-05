7b1086e1e631ff53e96bd014df45bca36a663f3d
elcis2614 <elisereflorbusole@gmail.com>
2023-10-05 16:09:58 +0000

    documenting the code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,22 @@
+#!/usr/bin/python3
+"""
+    method that determines if all the boxes can be opened
+"""
+
+
 def canUnlockAll(boxes):
+    """
+        Each box is numbered sequentially from 0 to n - 1 and
+        each box may contain keys to the other boxes.
+        returns true i fall boxes can be opened, else return False
+        The first box boxes[0] is unlocked
+    """
     gates = [0 for box in boxes]
     gates[0] = 1
 
     def openLock(addresses):
+        """ Given the keys(address) the function recursively
+            open boxes and updates the gates variables """
         if (addresses == []):
             return
         for address in addresses:
