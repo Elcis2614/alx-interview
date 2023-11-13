e57a609827048222a24619eeaa36bd2a70da32d8
elcis2614 <elisereflorbusole@gmail.com>
2023-11-13 01:24:20 +0000

    strawas api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`, function (error, response, body) {
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`, async function (error, response, body) {
   if (response.statusCode === 200) {
-    const content = JSON.parse(body).characters;
-    content.forEach((value, index, array) => {
-      request(value, (error, response, body) => {
-        if (response.statusCode === 200) {
-          const name = JSON.parse(body).name;
-          console.log(name);
-        } else {
-          console.log(error);
-        }
-      });
-    });
+    const content = await JSON.parse(body).characters;
+    for (const i in content) {
+      const ans = await printer(content[i]);
+      console.log(ans);
+    }
   } else {
     console.log(error);
   }
 });
+
+async function printer (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (response.statusCode === 200) {
+        const name = JSON.parse(body).name;
+        resolve(name);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
